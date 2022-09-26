e6ad393febdaed069ed5567666a7d972480a4f95
Shahidul Islam <shahid.okay@gmail.com>
2022-09-26 22:26:52 +0000

    Add signals dispather for user and user profile

--- user_profile/models.py
+++ user_profile/models.py
@@ -10,13 +10,13 @@ class UserProfile(models.Model):
     """
     user = models.OneToOneField(
         User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=100, unique=False)
-    last_name = models.CharField(max_length=100, unique=False)
-    email = models.EmailField(unique=True)
-    date_of_birth = models.DateField(unique=False)
+    first_name = models.CharField(max_length=100, unique=False, null=True)
+    last_name = models.CharField(max_length=100, unique=False, null=True)
+    email = models.EmailField(unique=True, null=True)
+    date_of_birth = models.DateField(unique=False, null=True)
     phone = models.IntegerField(null=True)
-    image = models.ImageField()
-    about_me = models.TextField(max_length=500)
+    image = models.ImageField(null=True, default='/static/images/audi.jpeg')
+    about_me = models.TextField(max_length=500, null=True)
 
     def __str__(self):
         return f'{self.user.username} Profile'
