7aa3f94b617227a88f78571d4b38d62741ad787b
Shahidul Islam <shahid.okay@gmail.com>
2022-10-05 16:34:47 +0000

    Remove hash from csp

--- mycar/settings.py
+++ mycar/settings.py
@@ -183,7 +183,7 @@ CSP_IMG_SRC = [
 CSP_SCRIPT_SRC = [
     'strict-dynamic',
     "'self' 'sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx'", 'https://stackpath.bootstrapcdn.com',
-    "'sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa'", 'https://cdn.jsdelivr.net',
+    'https://cdn.jsdelivr.net',
     "https://code.jquery.com",
     'https://kit.fontawesome.com'
 ]
@@ -191,7 +191,7 @@ CSP_SCRIPT_SRC = [
 CSP_INCLUDE_NONCE_IN = ['script-src']
 
 CSP_STYLE_SRC = [
-    "'unsafe-inline'", 'https://fonts.googleapis.com',
+    'https://fonts.googleapis.com',
     "'self'", 'https://www.bootstrapcdn.com',
     'https://cdn.jsdelivr.net',
     'https://cdnjs.cloudflare.com',
