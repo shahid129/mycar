91d104282191715a31dfecce63c87384087f2529
Shahidul Islam <shahid.okay@gmail.com>
2022-10-21 22:10:24 +0000

    Add docstrin, remove commented out code

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import PostAd, CustomerComment, Images
 from django_summernote.admin import SummernoteModelAdmin
+from .models import PostAd, CustomerComment, Images
 
 
 class ImagesAdmin(admin.StackedInline):
--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
 from django.utils.text import slugify
 
 # STATUS = ((0, "Draft"), (1, "Published"))
 
 
 class PostAd(models.Model):
+    """
+    A model that creates all the fields to post an add
+    """
     title = models.CharField(max_length=200, unique=True)
     price = models.IntegerField(null=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -21,19 +23,21 @@ class PostAd(models.Model):
     nct = models.DateField(auto_now=False)
     likes = models.ManyToManyField(
         User, related_name='blogpost_like', blank=True)
-    # Status = models.IntegerField(choices=STATUS, default=0)
 
     class Meta:
+        """
+        Order the listing by recently created items first
+        """
         ordering = ["-created_on"]
 
     def __str__(self):
         return self.title
 
     def number_of_likes(self):
+        """
+        Returns the total number of likes
+        """
         return self.likes.count()
-    
-    # def year_only(self):
-    #     return self.year.strftime('%Y')
 
     def save(self, *args, **kwargs):
         if not self.slug:
@@ -42,14 +46,20 @@ class PostAd(models.Model):
 
 
 class CustomerComment(models.Model):
-    post = models.ForeignKey(PostAd, on_delete=models.CASCADE, related_name="comments")
+    """
+    A model that creates all the fields for a customer to add comment
+    """
+    post = models.ForeignKey(
+        PostAd, on_delete=models.CASCADE, related_name="comments")
     name = models.CharField(max_length=100)
     body = models.TextField(max_length=500)
     created_on = models.DateTimeField(auto_now=True)
     approved = models.BooleanField(default=False)
 
-
     class Meta:
+        """
+        Order the Comments by recently created items first
+        """
         ordering = ['-created_on']
 
     def __str__(self):
@@ -60,5 +70,6 @@ class Images(models.Model):
     """
     Users can add more images along with their posts
     """
-    name = models.ForeignKey(PostAd, on_delete=models.CASCADE, blank=True, null=True)
+    name = models.ForeignKey(
+        PostAd, on_delete=models.CASCADE, blank=True, null=True)
     images = models.ImageField()
